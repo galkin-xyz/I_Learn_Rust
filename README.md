5d5d6606b0ab1946fcfa23ffec0d3ec977adf28d
Alexander Galkin <alexander.galkin@gmail.com>
2024-04-12 05:34:30 +0300

    Добавлена игра в загадки guessing_game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Ежедневная практика написания кода на языке Rust.
+
+**guessing_game** - игра в загадки из [второй главы книги "Язык программирования Rust"] (https://doc.rust-lang.ru/book/ch02-00-guessing-game-tutorial.html)
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Игра угадай число.");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(0..=100);
+//    println!("Загадано число {secret_number}.");
+
+    loop {
+        println!("Введите вашу догадку.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Не удалось получить значение.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Ожидается ввод числа!");
+                continue;
+            }
+        };
+        println!("Получено число {guess}.");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Введнное число меньше"),
+            Ordering::Greater=> println!("Введённое число больше."),
+            Ordering::Equal => {
+                println!("Вы угадали!");
+                break;
+            }
+        }
+    }
+}
