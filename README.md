cc6a189eb172e5a44d8f901400aba471bd19c536
Alexander Galkin <alexander.galkin@gmail.com>
2024-04-14 19:11:46 +0300

    Добавлена программа генерации n-го члена последовательности Фибоначчи.

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 **temperature_convertation** - конвертация температур между значениями по Фаренгейту к Цельсию. [Задание в конце третьей главы книги "Язык программирования Rust"](https://doc.rust-lang.ru/book/ch03-05-control-flow.html)
 
-**twelve_days** - вывод текста рождественской песни "Двенадцать дней Рождества" [Задание в конце третьей главы книги "Язык программирования Rust"](https://doc.rust-lang.ru/book/ch03-05-control-flow.html)
+**twelve_days** - вывод текста рождественской песни "Двенадцать дней Рождества"
+[Задание в конце третьей главы книги "Язык программирования Rust"](https://doc.rust-lang.ru/book/ch03-05-control-flow.html)
 ``` bash
 twelve_days > res.txt
 diff -s twelwe_days.txt res.txt 
 Файлы twelwe_days.txt и res.txt идентичны
 ```
+
+**fibonacci_sequence** - генерирование n-го числа Фибоначчи 
+[Задание в конце третьей главы книги "Язык программирования Rust"](https://doc.rust-lang.ru/book/ch03-05-control-flow.html)
 
---
+++ fibonacci_sequence/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+fn main() {
+    println!("Генерирование n-го числа Фибоначчи.");
+    println!("Формула для вычисления: F0 = 0, F1 = 1, Fn = Fn-1 + Fn-2 для n > 1.");
+
+    loop {
+        println!("Введите номер члена последовательности или <Enter> для выхода.");
+        let mut seq_number = String::new();
+        io::stdin().read_line(&mut seq_number).expect("Ошибка при чтении данных!");
+        let seq_number: u16 = match seq_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => break,
+        };
+    println!("Значение {} члена последовательности Фибоначчи равно {}", 
+             seq_number, fibonacci_number(seq_number));
+    }
+}
+
+fn fibonacci_number(n: u16) -> u32 {
+    let mut fn_2: u32 = 0;
+    let mut fn_1: u32 = 1;
+    let mut r#fn: u32 = fn_1 + fn_2;
+
+    match n {
+        0 => fn_2,
+        1 => fn_1,
+        2 => r#fn,
+        _ => {
+            for _ in 3..=n {
+                fn_2 = fn_1;
+                fn_1 = r#fn;
+                r#fn = fn_2 + fn_1;
+            };
+            r#fn
+        }
+    }
+}
