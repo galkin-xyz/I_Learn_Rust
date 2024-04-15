c75a8593ec7cf42ce87c4772ec3856fe10ccd4e9
Alexander Galkin <alexander.galkin@gmail.com>
2024-04-15 06:36:57 +0300

    Добавлено решение задачи 1293. Эния

--- toj-1293_enya/src/main.rs
+++ toj-1293_enya/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let mut params: [u32; 3] = [0, 0, 0];
+    let mut index: usize = 0;
+
+    io::stdin().read_line(&mut input).expect("Ошибка ввода.");
+    for word in input.split_whitespace() {
+        params[index] = word.parse().expect("Ошибка ввода.");
+        index += 1;
+    }
+    let res: u32 = 2 * params[0] * params[1] * params[2];
+    println!("{}", res);
 }
