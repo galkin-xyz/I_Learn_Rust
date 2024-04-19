bbd6f54ca1df6ecd30de399502576e00aab82e34
Alexander Galkin <alexander.galkin@gmail.com>
2024-04-19 17:32:14 +0300

    Добавить решение задачи 2001. Математики и ягоды.

--- toj-2001_maths_and_berries/src/main.rs
+++ toj-2001_maths_and_berries/src/main.rs
@@ -1,15 +1,20 @@
 use std::io;
 
 fn main() {
-    let mut params: [[u16; 2]; 3] = [[0; 2]; 3];
+    let mut weightings: [[u16; 2]; 4] = [[0; 2]; 4];
+    const MATH_A: usize = 0;
+    const MATH_B: usize = 1;
 
-    for weighing_num in 0..3 {
+    for weighing_num in 1..=3 {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Ошибка ввода.");
-        let mut math_num: usize = 0;
+        let mut math: usize = MATH_A;
         for word in input.split_whitespace() {
-            params[weighing_num][math_num] = word.trim().parse().expect("Ошибка данных.");
-            math_num += 1;
+            weightings[weighing_num][math] = word.trim().parse().expect("Ошибка данных.");
+            math += 1;
         }
     }
+    let math_a_berries_weight = weightings[1][MATH_A] - weightings[3][MATH_A];
+    let math_b_berries_weight = weightings[1][MATH_B] - weightings[2][MATH_B];
+    println!("{math_a_berries_weight} {math_b_berries_weight}");
 }
