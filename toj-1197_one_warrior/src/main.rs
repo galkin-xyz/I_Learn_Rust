a425a4c4129d62d52e9be62b15a1bd66d746d38c
Alexander Galkin <alexander.galkin@gmail.com>
2024-04-23 22:12:08 +0300

    Добавить решение задачи 1197. Один в поле воин. Сделать сделать функцию подсчёта возможных ходов

--- toj-1197_one_warrior/src/main.rs
+++ toj-1197_one_warrior/src/main.rs
@@ -8,6 +8,27 @@ fn main() {
     for _ in 0..tests_count {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Ошибка ввода.");
-        println!("{input}");
+        let vert_pos: u8 = match input.chars().next() {
+            Some('a') => 1,
+            Some('b') => 2,
+            Some('c') => 3,
+            Some('d') => 4,
+            Some('e') => 5,
+            Some('f') => 6,
+            Some('g') => 7,
+            Some('h') => 8,
+            _ => 0,
+        };
+        println!("{} - {}", vert_pos, &input[1..2]);
+    }
+}
+
+fn get_vert_movements_count(cur_vert_pos: u8, to_be_moved: u8) -> u8 {
+    if cur_vert_pos == 8 || cur_vert_pos == 1 {
+        1
+    } else if (cur_vert_pos == 7 || cur_vert_pos == 2) && to_be_moved == 2 {
+        1
+    } else {
+        2
     }
 }
