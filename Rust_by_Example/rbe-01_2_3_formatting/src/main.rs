1663ffe1c223e4dde1812562e6769378b637b416
Alexander Galkin <alexander.galkin@gmail.com>
2024-05-07 08:22:33 +0300

    Добавить пример 1.2.3. Formatting

---
+++ Rust_by_Example/rbe-01_2_3_formatting/src/main.rs
@@ -0,0 +1,49 @@
+use std::fmt::{self, Display, Formatter};
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(f, "{}: {:.3}°{} {:.3}°{}", 
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let rgb_hex: u32 = self.red as u32 * 65536 + self.green as u32 * 256 + self.blue as u32;
+        write!(f, "RGB ({R}, {G}, {B}) 0x{rgb_hex:0>6X}",
+            R = self.red, G = self.green, B = self.blue)
+    }
+}
+
+fn main() {
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 }
+    ] {
+        println!("{}", city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ] {
+        println!("{}", color);
+    }
+}
