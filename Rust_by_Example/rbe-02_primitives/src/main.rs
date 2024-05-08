95f1dc2e03fe09c2534e424a8493d0a7ac867bb2
Alexander Galkin <alexander.galkin@gmail.com>
2024-05-08 07:30:01 +0300

    Добавить пример 2. Primitives

---
+++ Rust_by_Example/rbe-02_primitives/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let logical: bool = true;
+   
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 3.0;
+    let default_int = 67;
+
+    // выведение типа переменной из контекста
+    let mut inferred_type = 12;
+    inferred_type = 4934603967i64;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    let mutable = true; // затенение переменной
+}
