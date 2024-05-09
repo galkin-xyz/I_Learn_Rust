9867e5d105283ed7b9b76dfec1edb92afb13f3a5
Alexander Galkin <alexander.galkin@gmail.com>
2024-05-09 20:28:24 +0300

    Добавить пример 2.1. Literals and operators

---
+++ Rust_by_Example/rbe-02_2_1_literals_and_operators/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("1 + 2 = {}", 1u32 + 2);
+    println!("1 - 2 = {}", 1i32 - 2);
+    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+    println!("Один миллион {}", 1_000_000u32);
+}
