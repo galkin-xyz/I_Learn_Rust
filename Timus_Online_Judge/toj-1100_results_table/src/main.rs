2c609e477e1fcdb875de1522577b65457ada213e
Alexander Galkin <alexander.galkin@gmail.com>
2024-05-26 08:43:22 +0300

    Добавить решение задачи 1100. Таблица результатов. Time limit exceeded on test #11

--- Timus_Online_Judge/toj-1100_results_table/src/main.rs
+++ Timus_Online_Judge/toj-1100_results_table/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 
 const MAX_TEAMS_COUNT: usize = 150000 + 1;
 
+#[derive(Clone, Copy)]
 struct Team {
     id: u32,
     score: u8,
@@ -10,15 +11,15 @@ struct Team {
 
 impl fmt::Display for Team {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "{} {}", self.id, self.score)
+        write!(f, "{} {}", self.id, self.score)
     }
 }
 
 fn main() {
     let mut teams_count = String::new();
     io::stdin().read_line(&mut teams_count).expect("Read error");
-    let teams_count: u32 = teams_count.trim().parse().expect("Data error");
-    let mut results_table: [Team; MAX_TEAMS_COUNT];
+    let teams_count: usize = teams_count.trim().parse().expect("Data error");
+    let mut results_table: [Team; MAX_TEAMS_COUNT] = [Team { id: 0, score: 0 }; MAX_TEAMS_COUNT];
 
     for index in 0..teams_count {
         let mut team_info = String::new();
@@ -36,5 +37,21 @@ fn main() {
             .trim()
             .parse()
             .expect("Data error");
+        results_table[index] = Team {
+            id: id,
+            score: score,
+        };
+    }
+    for j in 1..teams_count {
+        for i in 0..teams_count - j {
+            if results_table[i].score < results_table[i + 1].score {
+                let tmp = results_table[i + 1];
+                results_table[i + 1] = results_table[i];
+                results_table[i] = tmp;
+            }
+        }
+    }
+    for index in 0..teams_count {
+        println!("{}", results_table[index]);
     }
 }
