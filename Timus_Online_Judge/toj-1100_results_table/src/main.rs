0729e5e0e47b4075e40b17b9159bd155558a0e8f
Alexander Galkin <alexander.galkin@gmail.com>
2024-05-26 22:55:16 +0300

    Добавить решение задачи 1100. Таблица результатов.

--- Timus_Online_Judge/toj-1100_results_table/src/main.rs
+++ Timus_Online_Judge/toj-1100_results_table/src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 
-const MAX_TEAMS_COUNT: usize = 150000 + 1;
+//const MAX_TEAMS_COUNT: usize = 150000 + 1;
+const MAX_SCORE: usize = 100 + 1;
+const ARRAY_REPEAT_VALUE: Vec<u32> = Vec::new();
 
 fn main() {
     let mut teams_count = String::new();
     io::stdin().read_line(&mut teams_count).expect("Read error");
     let teams_count: usize = teams_count.trim().parse().expect("Data error");
-    let mut results_table: [(u32, u8); MAX_TEAMS_COUNT] = [(0, 0); MAX_TEAMS_COUNT];
+    let mut results_table: [Vec<u32>; MAX_SCORE] = [ARRAY_REPEAT_VALUE; MAX_SCORE];
 
-    for i in 0..teams_count {
+    for _ in 0..teams_count {
         let mut team_info = String::new();
         io::stdin().read_line(&mut team_info).expect("Read error");
         let mut iter = team_info.split_whitespace();
@@ -18,36 +20,18 @@ fn main() {
             .trim()
             .parse()
             .expect("Data error");
-        let score: u8 = iter
+        let score: usize = iter
             .next()
             .expect("Data error")
             .trim()
             .parse()
             .expect("Data error");
-        results_table[i] = (id, score);
+        results_table[score].push(id);
     }
-    // сортировка методом пузырька. Решение не проходит "Time limit exceeded"
-    //    for j in 1..teams_count {
-    //        for i in 0..teams_count - j {
-    //            if results_table[i].1 < results_table[i + 1].1 {
-    //                let tmp = results_table[i + 1];
-    //                results_table[i + 1] = results_table[i];
-    //                results_table[i] = tmp;
-    //            }
-    //        }
-    //    }
-    // сортировка методом вставки. Решение не проходит "Time limit exceeded"
-    //    for j in 1..teams_count {
-    //        let mut i = j;
-    //        while i > 0 && results_table[i].1 > results_table[i - 1].1 {
-    //                let tmp = results_table[i - 1];
-    //                results_table[i - 1] = results_table[i];
-    //                results_table[i] = tmp;
-    //                i -= 1;
-    //        }
-    //    }
 
-    for i in 0..teams_count {
-        println!("{} {}", results_table[i].0, results_table[i].1);
+    for i in (0..MAX_SCORE).rev() {
+        for j in 0..results_table[i].len() {
+            println!("{} {}", results_table[i][j], i);
+        }
     }
 }
