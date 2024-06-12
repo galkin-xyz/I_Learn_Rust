fc8f7905c3ee9bec00030ea1dc83df68970a11b9
Alexander Galkin <alexander.galkin@gmail.com>
2024-06-12 11:54:41 +0300

    Добавить решение задачи 1881. Длинное условие задачи

--- Timus_Online_Judge/toj-1881_long_task_condition/src/main.rs
+++ Timus_Online_Judge/toj-1881_long_task_condition/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Read error");
     let mut iter = input.split_whitespace();
-    let lines_per_page: u8 = iter
+    let lines_per_page: u16 = iter
         .next()
         .expect("Data error")
         .parse()
@@ -26,10 +26,28 @@ fn main() {
         io::stdin().read_line(&mut input).expect("Read error");
         len_of_words.push(input.trim().to_string().len() as u8);
     }
-
-    // check input
-    println!("{lines_per_page} {symbols_per_line} {words_in_text}");
-    for len in &len_of_words {
-        println!("{len}");
+    let mut lines_count: u16 = 0;
+    let mut index: usize = 0;
+    while index < len_of_words.len() {
+        let mut line_len: u8 = 0;
+        loop {
+            line_len += len_of_words[index] + 1;
+            if line_len - 1 > symbols_per_line {
+                break;
+            } else {
+                index += 1;
+            }
+            if index == len_of_words.len() {
+                break;
+            }
+        }
+        lines_count += 1;
     }
+    println!(
+        "{}",
+        match lines_count % lines_per_page {
+            0 => lines_count / lines_per_page,
+            _ => lines_count / lines_per_page + 1,
+        }
+    );
 }
