5209f7444cc3209c7dfea03a941151f2c4bf8fea
Alexander Galkin <alexander.galkin@gmail.com>
2024-06-16 20:43:40 +0300

    Решить Issue #5 по задаче 1293. Эния

--- Timus_Online_Judge/toj-1293_enya/src/main.rs
+++ Timus_Online_Judge/toj-1293_enya/src/main.rs
@@ -4,14 +4,14 @@ fn main() {
     const SIDE_COUNT: u32 = 2;
     const EXP: u32 = 1;
     let mut input = String::new();
-    let mut params: [u8; 3] = [0, 0, 0];
 
     io::stdin().read_line(&mut input).expect("Ошибка ввода.");
-    for (index, word) in input.split_whitespace().enumerate() {
-        params[index] = word.parse().expect("Ошибка ввода.");
-    }
-    println!(
-        "{}",
-        params[0] as u32 * params[1] as u32 * params[2] as u32 * SIDE_COUNT * EXP
-    );
+    let amount: u32 = input
+        .split_whitespace()
+        .take(3)
+        .map(|x| x.parse::<u32>().expect("Data error"))
+        .product::<u32>()
+        * SIDE_COUNT
+        * EXP;
+    println!("{amount}");
 }
