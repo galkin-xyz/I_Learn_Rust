ab9ce5df5259da7e0999b647674dbb31e96421f5
Alexander Galkin <alexander.galkin@gmail.com>
2024-06-17 22:10:17 +0300

    Решить Issue #8 по задаче 1264. Трудовые будни

--- Timus_Online_Judge/toj-1264_workdays/src/main.rs
+++ Timus_Online_Judge/toj-1264_workdays/src/main.rs
@@ -2,13 +2,14 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Ошибка ввода.");
+    io::stdin().read_line(&mut input).expect("Read error");
 
-    let mut iter = input.split_whitespace();
-    let array_size = iter.next().expect("Ошибка данных.");
-    let array_size: u16 = array_size.trim().parse().expect("Ошибка данных.");
-    let max_value = iter.next().expect("Ошибка данных.");
-    let max_value: u16 = max_value.trim().parse().expect("Ошибка данных.");
+    let mut iter = input
+        .split_whitespace()
+        .take(2)
+        .map(|x| x.trim().parse::<u32>().expect("Data error"));
+    let array_size: u32 = iter.next().expect("Data error");
+    let max_value: u32 = iter.next().expect("Data error");
 
-    println!("{}", array_size as u32 * (max_value as u32 + 1));
+    println!("{}", array_size * (max_value + 1));
 }
