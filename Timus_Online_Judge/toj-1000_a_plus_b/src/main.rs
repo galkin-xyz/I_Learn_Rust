93b48d2e61fa9602f1ea94ebbeed7ec477807ac8
Alexander Galkin <alexander.galkin@gmail.com>
2024-06-17 07:58:17 +0300

    Решить Issue #7 по задаче 1000. A+B Problem

--- Timus_Online_Judge/toj-1000_a_plus_b/src/main.rs
+++ Timus_Online_Judge/toj-1000_a_plus_b/src/main.rs
@@ -2,12 +2,12 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Ошибка ввода.");
+    io::stdin().read_line(&mut input).expect("Read error");
 
-    let mut sum: i32 = 0;
-    for word in input.split_whitespace() {
-        sum += word.parse::<i32>().expect("Ошибка данных.");
-    }
-
+    let sum: i32 = input
+        .split_whitespace()
+        .take(2)
+        .map(|x| x.parse::<i32>().expect("Data error"))
+        .sum();
     println!("{sum}");
 }
